2c7d5f125abba5b31836e3d236b633c538f5a9cd
David Álvarez <david11988@gmail.com>
2017-05-18 20:52:14 +0200

    Creamos un nuevo modulo y comunicamos ambos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  // selector css del elemento donde se instanciará el componente.
+  selector: 'app-root', 
+  // Ruta al template correspondiente al componente
   templateUrl: './app.component.html',
+  // Ruta al CS correspondiente al componente
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Vamos a desarrollar la agenda';
+export class AppComponent implements OnInit {
+
+  
+  private title: String;
+
+  // Este metodo es de obligatoria implementacion cuando usamos la interfaz OnINit. Puesto que no retorna nada, podemos anotarlo como 'void'. Este metodo se ejecuta al instanciarse la clase 'AppComponent'
+  ngOnInit(): void {
+    this.title = 'Vamos a desarrollar la agenda';
+  }
+
+  mostrarContactoSeleccionado(contacto: string): void {
+    alert(contacto);
+  }
+
 }
